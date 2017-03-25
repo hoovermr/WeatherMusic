cf73d6acc3e28e49845d7a3919165347c3e887cd
hoovermr <hoovermr@gmail.com>
2017-03-25 23:59:06 +0100

    fix play buttons

--- static/js/misc.js
+++ static/js/misc.js
@@ -1,10 +1,27 @@
-function EvalSound(soundobj, button) {
+var currentPlayer;
+var currentButton;
+function EvalSound(soundobj, button)
+{
     var thissound = document.getElementById(soundobj);
-    if (thissound.paused) {
-      thissound.play();
-      $(button).find(".glyphicon").removeClass("glyphicon-play").addClass("glyphicon-play");
-    } else {
-      thissound.pause();
-      $(button).find(".glyphicon").removeClass("glyphicon-pause").addClass("glyphicon-pause");
+    if(currentPlayer  && currentPlayer !== thissound)
+    {
+        currentPlayer.pause();
+        $(currentButton).find(".glyphicon").removeClass("glyphicon-pause").addClass("glyphicon-play");
     }
-  }
+    if (thissound.paused)
+    {
+        thissound.play();
+        currentPlayer = thissound;
+        currentButton = button;
+         if ($(button).find(".glyphicon").hasClass("glyphicon-play"))
+         {
+             $(button).find(".glyphicon").removeClass("glyphicon-play").addClass("glyphicon-pause");
+         }
+         else {$(button).find(".glyphicon").removeClass("glyphicon-pause").addClass("glyphicon-play");}
+    }
+    else
+    {
+        thissound.pause();
+        $(button).find(".glyphicon").removeClass("glyphicon-pause").addClass("glyphicon-play");
+    }
+}
